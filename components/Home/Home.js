e7ecf594a2dc56570ef25686b32832eef217f6de
pushp987pb <157052528+pushp987pb@users.noreply.github.com>
2024-03-12 00:39:13 +0530

    Home.js

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import styles from  './Home.css';
+import './Home.css';
 
 function Home() {
   return (
@@ -9,34 +9,47 @@ function Home() {
         {/* Replace the dummy image URL with your actual image URL */}
         <img
           className="hero-image"
-          src="https://images.unsplash.com/photo-1557610601-6f9750af7fb8?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hero Section"
+          src="https://pragyata.com/wp-content/uploads/2020/08/hindu-temple.jpg" alt="Hero Section"
         />
         <div className="hero-content">
-          <h1 className="hero-heading">Welcome to Our Temple Management System</h1>
+          <h1 className="hero-heading">Welcome to Our  Temple Management System</h1>
           {/* Add more content for the hero section if needed */}
         </div>
       </div>
 
-      <div className='container1'>
-        <h2 className='text-center display-6 lead text-primary'>Explore Our Temples</h2>
+      
         {/* Add more content for the main section of the page */}
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-             when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
-             centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-             when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
-             centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-             when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
-             centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-      </div>
+        <h2 className='text-center display-6 lead text-primary'>About Us</h2>
+        
+        <section class="stories">
+            <div class="stories-wrapper">
+                <div class="story-bg">
+                    <div class="story">
+                        <img src="https://cdn.pixabay.com/photo/2016/08/21/19/48/temple-1610623_640.jpg" alt="customer-image" class="story-image"/>
+                        <div class="story-text">
+                            <h1 class="story-heading">These were best days of this year</h1>
+                            <p class="story-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                               Architecto quas, repudiandae veritatis nam mollitia cumquedistinctio, 
+                               quia aperiam aliquid at consequuntur liberoquisquam facilis laborum inventore repellat perspiciatis vel 
+                               fugiat molestias recusandae eum necessitatibus quo possimusaspernatur? Nobis, architecto eaque.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="story-bg">
+                    <div class="story">
+                        <img src="https://cdn.pixabay.com/photo/2016/08/21/19/48/temple-1610623_640.jpg" alt="customer-image" class="story-image"/>
+                        <div class="story-text">
+                            <h1 class="story-heading">I enjoyes this great tour</h1>
+                            <p class="story-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                               Architecto quas, repudiandae veritatis nam mollitia cumquedistinctio,
+                                quia aperiam aliquid at consequuntur liberoquisquam facilis laborum inventore repellat 
+                                perspiciatis vel fugiat molestias recusandae eum necessitatibus quo possimusaspernatur? Nobis,
+                                 architecto eaque.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </>
   );
 }
